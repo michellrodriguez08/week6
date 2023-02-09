7a458b5387d13f9efd8be3e27cd46ac0059c0f1e
Michell Rodriguez <rodriguez.michell@student.greenriver.edu>
2023-02-09 00:51:09 -0500

    Build info page

--- index.php
+++ index.php
@@ -6,9 +6,6 @@
 ini_set('display errors', 1);
 error_reporting(E_ALL);
 
-////Start a session
-//session_start();
-
 //Require autoload file
 require_once ('vendor/autoload.php');
 
@@ -16,11 +13,32 @@ require_once ('vendor/autoload.php');
 $f3 = Base::instance();
 
 //Define a default route (328/diner)
-$f3->route('GET /', function() {
+$f3->route('GET /', function($f3) {
+
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with Templates');
+    $f3->set('food', 'Pizza');
+    $f3->set('color', 'Purple');
+    $f3->set('temp', 67);
+    $f3->set('radius', 10);
+
+//    Define an array of fruits
+    $fruits = array("apple", "banana", "orange");
+    $f3->set('fruits', $fruits);
+//    foreach ($fruits as $fruit) {
+//        echo $fruit;
+//    }
+
+    //other way to write arrays
+    $f3->set('colors', array("red", "blue", "yellow"));
+
+
+
    // echo "Week 6";
     //Instantiate a view
     $view = new Template();
-    echo $view->render("views/info.html");
+    echo $view->render('views/info.html');
 });
 
 //Run Fat Free
